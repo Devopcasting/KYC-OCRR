e2cf341cf3f7f9f109efa74ff01d554af721f2c5
devopcasting <devopcasting@gmail.com>
2024-03-22 18:32:50 +0530

    added exception

--- drivingl/document_info.py
+++ drivingl/document_info.py
@@ -28,164 +28,199 @@ class DrivingLicenseDocumentInfo:
                        'goa', 'gujarat', 'haryana', 'himachal pradesh', 'jammu and kashmir', 'jharkhand', 
                        'karnataka', 'kerala', 'lakshadweep (ut)', 'madhya pradesh', 'maharashtra', 'manipur', 
                        'meghalaya', 'mizoram', 'nagaland', 'odisha', 'puducherry (ut)', 'punjab', 'rajasthan', 
-                       'sikkim', 'tamil nadu', 'telangana', 'tripura', 'uttarakhand', 'uttar pradesh', 'mumbai', 'thane']
+                       'sikkim', 'tamil nadu', 'telangana', 'tripura', 'uttarakhand', 'uttar pradesh', 'mumbai',
+                       'chennai', 'delhi', 'new delhi', 'navi mumbai', 'thane']
     
-
     """func: extract driving license number"""
     def extract_dl_number(self):
-        result = {}
-        dl_number = ""
-        dl_number_coordinated = []
+        try:
+            result = {
+                "Driving License Number": "",
+                "coordinates": []    
+            }
+            dl_number = ""
+            dl_number_coordinated = []
 
-        """get the coordinates"""
-        for i,(x1, y1, x2, y2, text) in enumerate(self.coordinates):
-            if len(text) == 11 and text.isdigit():
-                dl_number = text
-                dl_number_coordinated.append([x1, y1, x2, y2])
-                break
-        if not dl_number_coordinated:
+            """get the coordinates"""
+            for i,(x1, y1, x2, y2, text) in enumerate(self.coordinates):
+                if len(text) == 11 and text.isdigit():
+                    dl_number = text
+                    dl_number_coordinated.append([x1, y1, x2, y2])
+                    break
+            if not dl_number_coordinated:
+                return result
+        
+            result = {
+                "Driving License Number": dl_number,
+                "coordinates": dl_number_coordinated
+            }
+            return result
+        except Exception as error:
             result = {
                 "Driving License Number": "",
                 "coordinates": []    
             }
             return result
-        
-        result = {
-            "Driving License Number": dl_number,
-            "coordinates": dl_number_coordinated
-        }
-
-        return result
-    
+            
     """func: extract dates"""
     def extract_dates(self):
-        result = {}
-        date_text = ""
-        date_coords = []
-        date_coordinates = []
+        try:
+            result = {
+                "Driving License Dates": "",
+                "coordinates": []
+            }
+            date_text = ""
+            date_coords = []
+            date_coordinates = []
 
-        """date pattern"""
-        date_pattern = r'\d{2}/\d{2}/\d{4}|\d{2}-\d{2}-\d{4}'
+            """date pattern"""
+            date_pattern = r'\d{2}/\d{2}/\d{4}|\d{2}-\d{2}-\d{4}'
 
-        """get the coordinates"""
-        for i, (x1,y1,x2,y2,text) in enumerate(self.coordinates):
-            date_match = re.search(date_pattern, text)
-            if date_match:
-                date_coords.append([x1, y1, x2, y2])
-                date_text += " "+ text
+            """get the coordinates"""
+            for i, (x1,y1,x2,y2,text) in enumerate(self.coordinates):
+                date_match = re.search(date_pattern, text)
+                if date_match:
+                    date_coords.append([x1, y1, x2, y2])
+                    date_text += " "+ text
         
-        if not date_coords:
+            if not date_coords:
+                return result
+        
+            """get the first 6 chars"""
+            for i in date_coords:
+                width = i[2] - i[0]
+                date_coordinates.append([i[0], i[1], i[0] + int(0.54 * width), i[3]])
+        
+            result = {
+                "Driving License Dates": date_text,
+                "coordinates": date_coordinates
+            }
+            return result
+        except Exception as error:
             result = {
                 "Driving License Dates": "",
                 "coordinates": []
             }
             return result
-        
-        """get the first 6 chars"""
-        for i in date_coords:
-            width = i[2] - i[0]
-            date_coordinates.append([i[0], i[1], i[0] + int(0.54 * width), i[3]])
-        
-        result = {
-            "Driving License Dates": date_text,
-            "coordinates": date_coordinates
-        }
 
-        return result
     
     """func: extract pincode"""
     def extract_pincode(self):
-        result = {}
-        pincode_number = ""
-        pincode_coordinates = []
-        pincode_coords = []
-
-        """get the coordinates"""
-        for i,(x1, y1, x2, y2, text) in enumerate(self.coordinates):
-            if len(text) == 6 and text.isdigit():
-                pincode_coords.append([x1, y1, x2, y2])
-                pincode_number += " "+text
-                break
-        if not pincode_coords:
+        try:
             result = {
                 "Driving License Pincode": "",
                 "coordinates": []
             }
-        for i in pincode_coords:
-            width = i[2] - i[0]
-            pincode_coordinates.append([i[0], i[1], i[0] + int(0.30 * width), i[3]])
-        
-        result = {
-            "Driving License Pincode": pincode_number,
-            "coordinates": pincode_coordinates
-        }
+            pincode_number = ""
+            pincode_coordinates = []
+            pincode_coords = []
 
-        return result
+            """get the coordinates"""
+            for i,(x1, y1, x2, y2, text) in enumerate(self.coordinates):
+                if len(text) == 6 and text.isdigit():
+                    pincode_coords.append([x1, y1, x2, y2])
+                    pincode_number += " "+text
+                    break
+            if not pincode_coords:
+                return result
+            
+            for i in pincode_coords:
+                width = i[2] - i[0]
+                pincode_coordinates.append([i[0], i[1], i[0] + int(0.30 * width), i[3]])
+        
+            result = {
+                "Driving License Pincode": pincode_number,
+                "coordinates": pincode_coordinates
+            }
+            return result
+        except Exception as error:
+            result = {
+                "Driving License Pincode": "",
+                "coordinates": []
+            }
+            return result
+
     
     """func: extract state"""
     def extract_state(self):
-        result = {}
-        state_name = ""
-        state_coordinates = []
-
-        """get the coordinates"""
-        for i,(x1, y1, x2, y2, text) in enumerate(self.coordinates):
-            if text.lower() in self.states:
-                state_coordinates.append([x1, y1, x2, y2])
-                state_name = text
-        
-        if not state_coordinates:
+        try:
             result = {
                 "Driving License Place": "",
                 "coordinates": []
             }
-        result = {
-            "Driving License Place": state_name,
-            "coordinates": state_coordinates
-        }
+            state_name = ""
+            state_coordinates = []
 
-        return result
+            """get the coordinates"""
+            for i,(x1, y1, x2, y2, text) in enumerate(self.coordinates):
+                if text.lower() in self.states:
+                    state_coordinates.append([x1, y1, x2, y2])
+                    state_name = text
+        
+            if not state_coordinates:
+                return result
+            
+            result = {
+                "Driving License Place": state_name,
+                "coordinates": state_coordinates
+            }
+            return result
+        except Exception as error:
+            result = {
+                "Driving License Place": "",
+                "coordinates": []
+            }
+            return result
+
     
     """func: extract name"""
     def extract_name(self):
-        result = {}
-        name_text = ""
-        name_coords = []
-        matching_text = r"\b(?:name)\b"
-        matching_text_index = 0
-
-        """get matching text index"""
-        for i,(x1, y1, x2, y2, text) in enumerate(self.coordinates):
-            if re.search(matching_text, text.lower(), flags=re.IGNORECASE):
-                matching_text_index = i
-                break
-        
-        if not matching_text_index:
+        try:
             result = {
                 "Driving License Name": "",
                 "coordinates": []
             }
+            name_text = ""
+            name_coords = []
+            matching_text = r"\b(?:name)\b"
+            matching_text_index = 0
 
-        """get the coordinates"""
-        for i in range(matching_text_index + 1, len(self.coordinates)):
-            text = self.coordinates[i][4]
-            if text.lower() in ['s/dmw', 'dmw', 's/']:
-                break
-            name_coords.append([x1, y1, x2, y2])
-            name_text += " "+text
+            """get matching text index"""
+            for i,(x1, y1, x2, y2, text) in enumerate(self.coordinates):
+                if re.search(matching_text, text.lower(), flags=re.IGNORECASE):
+                    matching_text_index = i
+                    break
         
-        if len(name_coords) > 1:
-            result = {
-                "Driving License Name": name_text,
-                "coordinates": [[name_coords[0][0], name_coords[0][1], name_coords[-1][2], name_coords[-1][3]]]
-            }
-        else:
-            result = {
-                "Driving License Name": name_text,
-                "coordinates": [[name_coords[0][0], name_coords[0][1], name_coords[0][2], name_coords[0][3]]]
-            }
+            if not matching_text_index:
+                return result
+
+            """get the coordinates"""
+            for i in range(matching_text_index + 1, len(self.coordinates)):
+                text = self.coordinates[i][4]
+                if text.lower() in ['s/dmw', 'dmw', 's/']:
+                    break
+                name_coords.append([x1, y1, x2, y2])
+                name_text += " "+text
         
-        return result
+            if len(name_coords) > 1:
+                result = {
+                    "Driving License Name": name_text,
+                    "coordinates": [[name_coords[0][0], name_coords[0][1], name_coords[-1][2], name_coords[-1][3]]]
+                }
+            else:
+                result = {
+                    "Driving License Name": name_text,
+                    "coordinates": [[name_coords[0][0], name_coords[0][1], name_coords[0][2], name_coords[0][3]]]
+                }
+        
+            return result
+        except Exception as error:
+            result = {
+                "Driving License Name": "",
+                "coordinates": []
+            }
+            return result
+
     
     """func: collect DL information"""
     def collect_dl_info(self):
